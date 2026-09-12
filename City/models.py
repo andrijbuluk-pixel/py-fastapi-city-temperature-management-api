f3edca83c9072139b0f1ad2f8eac4d15b40fbd54
Andriy <andrijbuluk@gmail.com>
2026-09-12 16:16:17 +0200

    Added unique for name and class following Base

--- City/models.py
+++ City/models.py
@@ -2,9 +2,9 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 
-class City(Base):
+class DBCity(Base):
     __tablename__ = "city"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255))
+    name = Column(String(255), unique=True)
     additional_info = Column(String(255), nullable=False)
