2c2e57ad9a2c8d6102096fd11db8772db1def0dd
Andriy <andrijbuluk@gmail.com>
2026-09-12 16:43:24 +0200

    Update models for line name default value and migrate

--- City/models.py
+++ City/models.py
@@ -6,5 +6,5 @@ class DBCity(Base):
     __tablename__ = "city"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), unique=True)
+    name = Column(String(255), unique=True, default="uq_city_name")
     additional_info = Column(String(255), nullable=False)
