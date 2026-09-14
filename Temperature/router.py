386e7f931c98bfc5b34edd0cca72dd7f178bf250
Andriy <andrijbuluk@gmail.com>
2026-09-14 08:37:19 +0200

    Added url for temperature post and get

---
+++ Temperature/router.py
@@ -0,0 +1,92 @@
+import httpx
+import asyncio
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal
+
+from Temperature import schemas
+from Temperature import crud
+
+
+router = APIRouter()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post(
+    "/temperatures/update/",
+    response_model=list[schemas.TemperatureAdded]
+)
+async def update_temperature(
+        db: Session = Depends(get_db)
+):
+    cities = crud.get_all_city(db=db)
+    updated_temperatures = []
+
+    async with httpx.AsyncClient() as client:
+        for city in cities:
+            await asyncio.sleep(5)
+
+            url_parameters_city = (
+                f"https://geocoding-api.open-meteo.com/v1/search?name={city.name}"
+            )
+            response = await client.get(url_parameters_city)
+            geo = response.json()
+
+            if not geo.get("results"):
+                continue
+
+            lat = geo["results"][0]["latitude"]
+            lon = geo["results"][0]["longitude"]
+
+            params = {
+                "latitude": lat,
+                "longitude": lon,
+                "current": "temperature_2m",
+            }
+
+            weather_response = await client.get(
+                "https://api.open-meteo.com/v1/forecast/",
+                params=params
+            )
+            weather_json = weather_response.json()
+
+            current_temperature = weather_json["current"]["temperature_2m"]
+            current_time = weather_json["current"]["time"]
+
+            temperature_date = schemas.TemperatureAdded(
+                city_id=city.id,
+                date_time=current_time,
+                temperature=current_temperature,
+            )
+
+            save_temperature = crud.update_temperature_city(
+                db=db,
+                temperature=temperature_date
+            )
+            updated_temperatures.append(save_temperature)
+
+        return updated_temperatures
+
+
+@router.get("/temperatures", response_model=list[schemas.Temperature])
+def get_temperatures(
+        db: Session = Depends(get_db)
+) -> list[schemas.Temperature]:
+    return crud.get_all_temperature(db=db)
+
+
+@router.get("/temperatures/{city_id}", response_model=schemas.Temperature)
+def get_temperature_id(
+        city_id: int,
+        db: Session = Depends(get_db)
+):
+    return crud.get_temperature_by_city_id(db=db, city_id=city_id)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from City import router as CityRouter
+from Temperature import router as TemperatureRouter
 
 app = FastAPI()
 
 app.include_router(CityRouter.router)
+app.include_router(TemperatureRouter.router)
 
 @app.get("/")
 def read_root():
